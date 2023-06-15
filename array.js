d2e5e34989693f3b4e355f8dc42f03413a786d1a
eshikhon-mern-stack <mern2.eshikhon@gmail.com>
2023-06-15 20:51:39 +0600

    first commit

---
+++ array.js
@@ -0,0 +1,199 @@
+var name1 = ["Mitu", "Rabeya", "shitol", "hima"];
+//console.log(name1);
+//console.log(name1.length);
+//console.log(name1.indexOf("shitol"));
+
+
+var number=[25, 26, 27, 28, 29 ];
+//console.log(number);
+//console.log(number.length);
+//console.log(number.indexOf(28));
+
+var even_number=[12, 14, 16, 18, 20 ];
+//console.log(even_number);
+//console.log(even_number.length);
+//console.log(even_number.indexOf(12));
+
+var odd_number=[5, 7, 9, 11, 13 ];
+//console.log(odd_number);
+//console.log(odd_number.length);
+//console.log(odd_number.indexOf(13));
+
+var vowels= ["a", "e", "i", "o", "u"];
+//console.log(vowels);
+//console.log(vowels.length);
+//console.log(vowels.indexOf("i"));
+
+var myFavouritBooks = ["Thinking", "love apart", "Morning Diary", "memory" ];
+//console.log(myFavouritBooks);
+myFavouritBooks[0]="sunset";
+//console.log(myFavouritBooks.indexOf("sunset"));
+indexofSunset=myFavouritBooks.indexOf("sunset")
+//console.log(indexofSunset);
+myFavouritBooks[indexofSunset]="Thinking";
+//console.log(myFavouritBooks);
+
+var students =["Moriom","Nupur", "sadiya" ]
+//console.log(students);
+//students.push("Anamika");
+//students.pop();
+//console.log(students);
+//students.unshift("anamika");
+//console.log(students);
+//students.shift();
+//console.log(students);
+
+//console.log(10>11)
+//console.log(10<11)
+//console.log(10==11)
+//console.log(10!=11)
+
+var moneyofsakib = 1000;
+var moneyofrakib = 1500;
+//console.log(moneyofrakib>moneyofsakib);
+//console.log(moneyofrakib<moneyofsakib);
+//console.log(moneyofrakib==moneyofsakib);
+//console.log(moneyofrakib!=moneyofsakib);
+
+var sakibjobholder = true;
+var sakiblookhandsome = true;
+var sakibprogrammer = false;
+var sakibsalary=4000;
+//console.log(sakibjobholder&& sakiblookhandsome && sakibprogrammer && sakibsalary>10000);
+//console.log(sakibprogrammer|| sakibsalary==4000||sakibjobholder);
+
+var sakibjobholder = true;
+var sakiblookhandsome = true;
+var sakibprogrammer = true;
+var sakibsalary=10000;
+//console.log(sakibjobholder&& sakiblookhandsome && sakibprogrammer && sakibsalary>9000);
+
+
+var heget_50 = true;
+//if(heget_50){
+  //  console.log("he pass");
+//}
+//else{
+  //  console.log("he fail");
+//}
+var heget_50 = false;
+//if(heget_50){
+  //  console.log("he pass");
+//}
+//else{
+  //  console.log("he fail");
+//}
+
+var it_is_raining=false;
+var it_is_not_raining=true;
+//if(it_is_raining){
+//    console.log("I will sleep");
+//}
+ //else if(it_is_not_raining){
+ //   console.log("I will not go to school");
+//}
+//else {
+//    console.log("I will go to school");
+//}
+
+var it_is_raining=true;
+var it_is_not_raining=false;
+//if(it_is_raining){
+//    console.log("I will sleep");
+//}
+ //else if(it_is_not_raining){
+ //   console.log("I will not go to school");
+//}
+//else {
+//    console.log("I will go to school");
+//}
+
+var it_is_raining=false;
+var it_is_not_raining=false;
+//if(it_is_raining){
+//    console.log("I will sleep");
+//}
+ //else if(it_is_not_raining){
+ //   console.log("I will not go to school");
+//}
+//else {
+//    console.log("I will go to school");
+//}
+
+var my_pocketmoney = 300;
+var halim=400;
+var ramen = 400;
+var chotpoti = 50;
+var ruti =20;
+//if (my_pocketmoney==halim){
+//    console.log("Ami Halim khabo");
+//}
+//else if(my_pocketmoney==ramen)
+//{
+//    console.log("ami ramen khabo");
+//}
+//else if(my_pocketmoney>chotpoti && my_pocketmoney>ruti)
+//{
+//    console.log("Ami chootpoti and ruti khabo");
+//}
+//else{
+//    console.log("Ami ruti khabo");
+//}
+
+var my_pocketmoney = 300;
+var halim=400;
+var ramen = 300;
+var chotpoti = 50;
+var ruti =20;
+//if (my_pocketmoney==halim){
+//    console.log("Ami Halim khabo");
+//}
+//else if(my_pocketmoney==ramen)
+//{
+//    console.log("ami ramen khabo");
+//}
+//else if(my_pocketmoney==chotpoti)
+//{
+//    console.log("Ami chootpoti and ruti khabo");
+//}
+//else{
+//    console.log("Ami ruti khabo");
+//}
+
+var my_pocketmoney = 300;
+var halim=300;
+var ramen = 200;
+var chotpoti = 50;
+var ruti =20;
+//if (my_pocketmoney==halim){
+//    console.log("Ami Halim khabo");
+//}
+//else if(my_pocketmoney==ramen)
+//{
+//    console.log("ami ramen khabo");
+//}
+//else if(my_pocketmoney>chotpoti && my_pocketmoney>ruti)
+//{
+//    console.log("Ami chootpoti and ruti khabo");
+//}
+//else{
+//    console.log("Ami ruti khabo");
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
